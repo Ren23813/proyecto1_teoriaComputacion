b6a1f9812ece2de4ea54e02b0be2f86737d48d57
Renato Rojas <roj23813@uvg.edu.gt>
2025-09-02 00:25:03 -0600

    agregado export a JSON

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Se determinó que el épsilon (cadena vacía) será un '?'
- 
+import json
+
 def verificar_input(input:str):
     traduccion = str.maketrans('áéíóúüÁÉÍÓÚÜ','aeiouuAEIOUU')
     input.translate(traduccion)
@@ -9,5 +10,26 @@ def verificar_input(input:str):
     print(f"Usted ingresó {input}, y su valor de aceptación es:", verdad)
     #tal vez verificar que todos los paréntesis estén en pares? (cerrados y abiertos)
 
-verificar_input("aee1515+++*****???|||||()()()()()")
+verificar_input("aee1515+++*****???|||||()()()()()") #ejemplo
 
+def export_automata(estados,simbolos,inicio,aceptacion,transiciones):
+    data = {
+        "estados": estados,
+        "simbolos": simbolos,
+        "inicio": inicio,
+        "aceptacion": aceptacion,
+        "transiciones": transiciones
+    }
+    #Se exportará a JSON
+    with open("automata.json", "w") as json_file:
+        json.dump(data, json_file, indent=3)
+
+#ejemplo 000000000000000000000000000000000000000000000000000000
+estados = [0, 1, 2]
+simbolos = ['a', 'b']
+inicio = 0
+aceptacion = [1]
+transiciones = [(0, 'a', 1), (1, 'b', 2), (2, 'a', 0)]
+
+export_automata(estados,simbolos,inicio,aceptacion,transiciones)
+#ejemplo 111111111111111111111111111111111111111111111111111111
