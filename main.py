7f5e189b8b0f6220a82e64a4ff6cd0ec88e0a1fa
Renato Rojas <roj23813@uvg.edu.gt>
2025-09-06 21:21:11 -0600

    implementacion visual

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Se determinó que el épsilon (cadena vacía) será un '?'
 import json
-from primeraMitad import shunting_yard
+from primeraMitad import shunting_yard,regex_AFN_McYamadaSon
+from automathon import NFA
 
 #estructura a seguir de automatas
 automata = {
@@ -23,7 +24,6 @@ def verificar_input(input:str):
     print(f"Usted ingresó {input}, y su valor de aceptación es:", verdad)
     return verdad
     #tal vez verificar que todos los paréntesis estén en pares? (cerrados y abiertos)
-
 # verificar_input("aee1515+++*****???|||||()()()()()") #ejemplo
 
 def export_automata(estados,simbolos,inicio,aceptacion,transiciones):
@@ -37,15 +37,44 @@ def export_automata(estados,simbolos,inicio,aceptacion,transiciones):
     #Se exportará a JSON
     with open("automata.json", "w") as json_file:
         json.dump(data, json_file, indent=3)
-
-#ejemplo 000000000000000000000000000000000000000000000000000000
+#ejemplo
 # estados = [0, 1, 2]
 # simbolos = ['a', 'b']
 # inicio = 0
 # aceptacion = [1]
 # transiciones = [(0, 'a', 1), (1, 'b', 2), (2, 'a', 0)]
 # export_automata(estados,simbolos,inicio,aceptacion,transiciones)
-#ejemplo 111111111111111111111111111111111111111111111111111111
+#/ejemplo
+# automata_export = regex_AFN_McYamadaSon(shunting_yard("(0.0|1)*.1.(0|1)"))
+# export_automata(automata_export["estados"],automata_export["simbolos"],automata_export["inicio"],automata_export["aceptacion"],automata_export["transiciones"])
 
-# shunting_yard("3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3")
- 
+def visualize_automata(mi_auto, file_name="automata"):
+    q = set(mi_auto["estados"])
+    #Automathon solo permite la cadena vacía '' para epsilon,
+    sigma = set()
+    for s in mi_auto["simbolos"]:
+        if s == '?':
+            sigma.add('')   # interpretacion de epsilon en libreria
+        else:
+            sigma.add(s)
+    
+    delta = {estado: {} for estado in q}
+    for origen, simbolo, destino in mi_auto["transiciones"]:
+        sym = '' if simbolo == '?' else simbolo
+        if origen not in delta:
+            delta[origen] = {}
+        delta[origen].setdefault(sym, set()).add(destino)
+    
+    initial_state = mi_auto["inicio"]
+    f = set(mi_auto["aceptacion"])  # conjunto de finales
+    
+    nfa = NFA(q=q, sigma=sigma, delta=delta, initial_state=initial_state, f=f)
+    
+    try:
+        print("Validando NFA:", nfa.is_valid())
+    except Exception as e:
+        print("Advertencia: is_valid() lanzó excepción:", e)
+    nfa.view(file_name)
+    print(f"Visualización guardada como {file_name}.")
+
+# visualize_automata(regex_AFN_McYamadaSon(shunting_yard("(0.0|1)*.1.(0|1)")),"prueba")
